5d39c8449f797435501d78ab5dc360421fb1af56
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-06 16:10:57 +0700

    Commit 5
    Copy code in passport.cpp
    (class Passport)
    Create class FPassport (Foreign Passport)

---
+++ Passport/Passport/passport.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Passport {
+protected:
+	string name{};
+	string sname{};
+	unsigned int dr[3]{ 1,1,1900 };
+	static unsigned int np;
+public:
+	Passport(string nameP, string snameP, unsigned int dP, unsigned int mP, unsigned int yP) {
+		this->name = nameP;
+		this->sname = snameP;
+		this->dr[0] = dP;
+		this->dr[1] = mP;
+		this->dr[2] = yP;
+		this->np++;
+	}
+	virtual void show_passport() {
+		cout << "Bangladesh Passport:\n";
+		cout << "Surname - " << this->sname << endl;
+		cout << "Name - " << this->name << endl;
+		cout << "Date of Birth - " << this->dr[0] << "." << this->dr[1] << "." << this->dr[2] << endl;
+		cout << "Serial number - " << this->np << endl << endl;
+	}
+};
+
+class Fpassport : public Passport {
+	static unsigned int np_f;
+	string visa_arr[10]{};
+	unsigned int visa_full{ 0 };
+};
+
+unsigned int Passport::np{ 0 }; 
+unsigned int Fpassport::np_f{ 0 };
+
+int main() {
+
+
+	return 0;
+}
