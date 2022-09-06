ce98e861797a556b72d1a358464e28c369538cab
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-06 16:23:29 +0700

    Commit 6
    Add default constructor  to Passport
    Add constructor to Fpassport and virtual metod to show

--- Passport/Passport/passport.cpp
+++ Passport/Passport/passport.cpp
@@ -18,6 +18,7 @@ public:
 		this->dr[2] = yP;
 		this->np++;
 	}
+	Passport() {}
 	virtual void show_passport() {
 		cout << "Bangladesh Passport:\n";
 		cout << "Surname - " << this->sname << endl;
@@ -31,13 +32,29 @@ class Fpassport : public Passport {
 	static unsigned int np_f;
 	string visa_arr[10]{};
 	unsigned int visa_full{ 0 };
+public:
+	Fpassport(string nameP, string snameP, unsigned int dP, unsigned int mP, unsigned int yP) {
+		this->name = nameP;
+		this->sname = snameP;
+		this->dr[0] = dP;
+		this->dr[1] = mP;
+		this->dr[2] = yP;
+		this->np_f++;
+	}
+	virtual void show_passport() {
+		cout << "Bangladesh Foreign Passport:\n";
+		cout << "Surname - " << this->sname << endl;
+		cout << "Name - " << this->name << endl;
+		cout << "Date of Birth - " << this->dr[0] << "." << this->dr[1] << "." << this->dr[2] << endl;
+		cout << "Serial number - " << this->np << endl << endl;
+	}
 };
 
-unsigned int Passport::np{ 0 }; 
+unsigned int Passport::np{ 0 };
 unsigned int Fpassport::np_f{ 0 };
 
 int main() {
 
-
+	
 	return 0;
 }
