1b159a495a2385e5c6263aab551dcf4ca86726e8
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 12:41:25 +0700

    Commit 10
    Create template Class List
    Add constructor and destructor to List

---
+++ list_stack_queue/list_stack_queue/List.cpp
@@ -0,0 +1,16 @@
+﻿#include "List.h"
+#include<iostream>
+
+using namespace std;
+
+// конструктор с параметром, он же по умолчанию
+template <typename T>
+List<T>::List(const int l) {
+	this->max_l = l;
+	this->arr = new T [this->max_l];
+}
+// деструктор
+template <typename T>
+List<T>::~List() {
+	delete[] this->arr;
+}
---
+++ list_stack_queue/list_stack_queue/List.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+// шаблон класса Список
+template <typename T>
+class List
+{
+protected:
+	T* arr;
+	int max_l{ 0 };
+	int last{ 0 };
+public:
+// описание конструктора и деструктора
+	List(const int l);
+	~List();
+};
+
