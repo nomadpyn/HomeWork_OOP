db46bfaa064a78a4d6027010f9d91859f8a1e0af
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 17:01:22 +0700

    Commit 17
    Create class Transmission

---
+++ class_Auto/class_Auto/Transmission.cpp
@@ -0,0 +1,10 @@
+﻿#include "Transmission.h"
+#include<iostream>
+#include<string>
+
+// виртуальный метод показа информации о двигатель на экран
+void Transmission::show_info() const {
+	cout << "Transmission name: " << this->Tname << ". ";
+	cout << "Type: " << this->Type<< ". ";
+	cout << "How many speeds: " << this->Speeds << ".\n";
+}
---
+++ class_Auto/class_Auto/Transmission.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<string>
+using namespace std;
+
+// класс кпп
+class Transmission
+{
+protected:
+	string Tname{ 0 };
+	string Type{ 0 };
+	short Speeds{ 0 };
+public:
+	// конструкторы с параметрами и по умолчанию
+	Transmission(string n, string t, short s) : Tname{ n }, Type{ t }, Speeds{ s } {}
+	Transmission() {}
+	// описание виртуального метода показа информации о двигатель на экран
+	virtual void show_info() const;
+};
