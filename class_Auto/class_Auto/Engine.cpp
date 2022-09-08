71dd36ad4d3da3867a32086f254129e11b83da40
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 16:57:30 +0700

    Commit 16
    Create class Engine

---
+++ class_Auto/class_Auto/Engine.cpp
@@ -0,0 +1,10 @@
+﻿#include "Engine.h"
+#include<iostream>
+#include<string>
+
+// виртуальный метод показа информации о двигатель на экран
+void Engine::show_info() const {
+	cout << "Engine name: " << this->Ename << ". ";
+	cout << "Volume: " << this->Volume << " L. ";
+	cout << "Power: " << this->Power << " HP.\n";
+}
---
+++ class_Auto/class_Auto/Engine.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include<string>
+using namespace std;
+
+// класс двигатель
+class Engine
+{
+protected:
+	string Ename{0};
+	double Volume{0};
+	short Power{0};
+public:
+// конструкторы с параметрами и по умолчанию
+	Engine(string n, double v, short p): Ename{n},Volume{v},Power{p}{}
+	Engine(){}
+// описание виртуального метода показа информации о двигатель на экран
+	virtual void show_info() const;
+};
+
