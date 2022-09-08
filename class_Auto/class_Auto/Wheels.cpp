c90fd65997c8d269e7fd36c4beedf96390793679
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 17:10:47 +0700

    Commit 19
    Create class Wheels

---
+++ class_Auto/class_Auto/Wheels.cpp
@@ -0,0 +1,10 @@
+﻿#include "Wheels.h"
+#include<iostream>
+#include<string>
+
+// виртуальный метод показа информации о колесах на экран
+void Wheels::show_info() const {
+	cout << "Wheels name: " << this->Wname << ". ";
+	cout << this->diam << " R. ";
+	cout << this->amount << "wheels.\n";
+}
---
+++ class_Auto/class_Auto/Wheels.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<string>
+using namespace std;
+
+// класс колеса
+class Wheels
+{
+protected:
+	string Wname{ 0 };
+	short diam{ 0 };
+	short amount{ 0 };
+public:
+	// конструкторы с параметрами и по умолчанию
+	Wheels(string n, short d, short a) : Wname{ n }, diam{ d },amount{ a } {}
+	Wheels() {}
+	// описание виртуального метода показа информации о колесах на экран
+	virtual void show_info() const;
+};
