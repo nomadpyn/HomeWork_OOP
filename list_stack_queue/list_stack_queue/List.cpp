0ff650fbfc6283cd7d23bdb54d77e35b99ae7d30
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 12:57:37 +0700

    Commit 11
    Add metod to clear list, check for empty and full, get count of elements in list

--- list_stack_queue/list_stack_queue/List.cpp
+++ list_stack_queue/list_stack_queue/List.cpp
@@ -14,3 +14,25 @@ template <typename T>
 List<T>::~List() {
 	delete[] this->arr;
 }
+// метод очистки списка
+template <typename T>
+void List<T>::Clear() {
+	delete[] this->arr;
+	arr = new T[this->max_l];
+	last = -1;
+}
+// метод проверки на пустоту списка
+template <typename T>
+bool List<T>::isEmpty() const {
+	return last == -1;
+}
+// метод проверки на полный список
+template <typename T>
+bool List<T>::isFull() const{
+	return last == max_l-1;
+}
+// метод вывода количества элементов в списке
+template <typename T>
+int List<T>::GetCount() const {
+	return last + 1;
+}
--- list_stack_queue/list_stack_queue/List.h
+++ list_stack_queue/list_stack_queue/List.h
@@ -6,10 +6,18 @@ class List
 protected:
 	T* arr;
 	int max_l{ 0 };
-	int last{ 0 };
+	int last{ -1 };
 public:
 // описание конструктора и деструктора
 	List(const int l);
 	~List();
+// описание методов класса
+	virtual void Clear();
+	virtual bool isEmpty() const;
+	virtual bool isFull() const;
+	virtual int GetCount() const;
+	virtual void Add(const T elm,const int pos);
+	virtual T Pop();
+	
 };
 
