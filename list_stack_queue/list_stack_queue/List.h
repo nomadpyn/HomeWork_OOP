caf6bc36d608886e916e11593c7ba95cc846aba2
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-08 13:37:40 +0700

    Commit 12
    Finish class List
    Remove all metod to cpp, delete h

--- list_stack_queue/list_stack_queue/List.h
+++ list_stack_queue/list_stack_queue/List.h
@@ -1,4 +1,8 @@
 ﻿#pragma once
+#include<iostream>
+
+using namespace std;
+
 // шаблон класса Список
 template <typename T>
 class List
@@ -8,16 +12,52 @@ protected:
 	int max_l{ 0 };
 	int last{ -1 };
 public:
-// описание конструктора и деструктора
-	List(const int l);
-	~List();
-// описание методов класса
-	virtual void Clear();
-	virtual bool isEmpty() const;
-	virtual bool isFull() const;
-	virtual int GetCount() const;
-	virtual void Add(const T elm,const int pos);
-	virtual T Pop();
-	
+	// конструктор с параметром, он же по умолчанию
+	List(int l) {
+		this->max_l = l;
+		this->arr = new T[l];
+	}
+	// деструктор
+	~List() {
+		delete[] this->arr;
+	}
+	// метод очистки списка
+	virtual void Clear() {
+		delete[] this->arr;
+		arr = new T[this->max_l];
+		last = -1;
+	};
+	// метод проверки на пустоту списка
+	virtual bool isEmpty() const {
+		return last == -1;
+	};
+	// метод проверки на полный список
+	virtual bool isFull() {
+		return last == max_l;
+	};
+	// метод вывода количества элементов в списке
+	virtual int GetCount() {
+		return last + 1;
+	};
+	// метод добавлния элемента в список
+	virtual void Add(const T elm) {
+		if (!isFull()) {
+			last++;
+			this->arr[last] = elm;
+		}
+	};
+	// метод выдачи определенного элемента из списка
+	virtual T Pop(const int pos) {
+		if (pos > 0 && pos < this->max_l)
+			return this->arr[pos - 1];
+		else
+			return this->arr[last];
+	};
+	// метод вывода всего списка на экран
+	virtual void Show() {
+		for (int i = 0; i < last+1; i++) {
+			cout << this->arr[i] << " ";
+		}
+		cout << endl;
+	};
 };
-
--- list_stack_queue/list_stack_queue/main.cpp
+++ list_stack_queue/list_stack_queue/main.cpp
@@ -1,8 +1,10 @@
 ﻿#include<iostream>
+#include"List.h"
 
 using namespace std;
 
 int main() {
+
 
 	return 0;
 }
