8d91b1467001b67e6df9a50c609e40b1341d2e34
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-09 12:28:14 +0700

    Commit 25
    Add class Hamster

---
+++ class_Pet/class_Pet/Hamster.cpp
@@ -0,0 +1,17 @@
+﻿#include "Hamster.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+// метод вывода имени животного
+void Hamster::show() const {
+	cout << "Hamster name - " << this->name << endl;
+}
+// метод вывода подвида животного
+void Hamster::type() const {
+	cout << "Subspecies of pet is " << this->subspecies << ". Color is " << this->color << endl;
+}
+// метод "голос" животного
+void Hamster::sound() const {
+	cout << this->name << " say Pi Pi Pi\n";
+}
---
+++ class_Pet/class_Pet/Hamster.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include<string>
+#include"Pet.h"
+// класс Хомяк
+class Hamster : public Pet
+{
+public:
+	// конструктор по умолчанию
+	Hamster(std::string n, std::string t, std::string c) : Pet(n) {
+		this->subspecies = t;
+		this->color = c;
+	}
+	// описание методов
+	virtual void show() const;
+	virtual void sound() const;
+	virtual void type() const;
+};
