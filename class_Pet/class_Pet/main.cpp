dd0d3cb3fec78fce42d407c7ed0e61b346577fbb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-09 12:32:45 +0700

    Commit 26
    Update main in Pet

--- class_Pet/class_Pet/main.cpp
+++ class_Pet/class_Pet/main.cpp
@@ -1,7 +1,26 @@
 ﻿#include<iostream>
 #include"Dog.h"
+#include"Cat.h"
+#include"Parrot.h"
+#include"Hamster.h"
+
 using namespace std;
 
 void main() {
-	
+	Dog A("Barbos", "Dvorniaga", "Black");
+	A.show();
+	A.type();
+	A.sound();
+	Cat B("Murka", "Britain", "Grey");
+	B.show();
+	B.type();
+	B.sound();
+	Parrot C("Boris", "Yara", "Green");
+	C.show();
+	C.type();
+	C.sound();
+	Hamster D("Dima", "Svinka", "Yellow");
+	D.show();
+	D.type();
+	D.sound();
 }
