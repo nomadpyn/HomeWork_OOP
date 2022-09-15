7a515fa3e3adffde4496a80f42be5cd62eb67853
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-09-15 19:04:16 +0700

    Commit 28
    Create struct Elem
    Create class Tree for binary Tree

---
+++ fine_traffic_police/fine_traffic_police/Tree.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include"func.h"
+// класс бинарное дерево
+class Tree
+{
+	Elem* root;
+public:
+// конструктор и деструктор
+	Tree();
+	~Tree();
+// описание методов класса 
+	void print(Elem* Node);
+	Elem* search(Elem* Node, string key);
+	Elem* Min(Elem* Node);
+	Elem* Max(Elem* Node);
+	Elem* Next(Elem* Node);
+	Elem* Previous(Elem* Node);
+	void Insert(Elem* nd);
+	void Del(Elem* nd);
+	Elem* GetRoot();
+};
+
---
+++ fine_traffic_police/fine_traffic_police/func.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+
+using namespace std;
+// структура для создания элемента бинарного дерева
+struct Elem {
+	string number{};
+	vector<string> fines{};
+	Elem* right, * left, * parent;
+};
