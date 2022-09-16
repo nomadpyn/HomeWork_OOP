b9b252c3fa62225e9e3cb1a35d16b202872b8ec5
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-09-16 15:02:17 +0700

    Commit 30
    Add metod to show all tree in console

--- fine_traffic_police/fine_traffic_police/Tree.cpp
+++ fine_traffic_police/fine_traffic_police/Tree.cpp
@@ -11,3 +11,14 @@ Tree::Tree() {
 Tree::~Tree() {
 	Del();
 }
+// метод вывода всего дерева на экран
+void Tree::print(Elem * Node) {
+	if (Node != 0) {
+		print(Node->left);
+		cout << Node->number << endl;
+		for (int i = 0; i < Node->fines.size(); i++)
+			cout << Node->fines[i] << " ";
+		cout << endl;
+		print(Node->right);
+	}
+}
--- fine_traffic_police/fine_traffic_police/main.cpp
+++ fine_traffic_police/fine_traffic_police/main.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include<iostream>
+#include<string>
+#include"func.h"
+#include"Tree.h"
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
