63650a62debd23c31e0ceef58f634202fedc2532
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-20 16:14:23 +0700

    Commit 42
    Add func to search by name in file

--- class_Directory/class_Directory/func.h
+++ class_Directory/class_Directory/func.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include<iostream>
 #include<fstream>
+#include<string>
 #include"Directory.h"
 
 using namespace std;
@@ -36,6 +37,30 @@ void showFromFile() {
 	for (int i = 0; i < count;i++) {
 		fl.read(reinterpret_cast<char*>(&temp), sizeof(Directory));
 		temp.show();
+	}
+	fl.close();
+}
+// фукнция поиска в справочнике по имени организации
+void searchName(const char name[64]) {
+	ifstream fl("data.txt", ios::in | ios::binary);
+	int count = howManyObj();
+	Directory temp;
+	char name_t[64]{};
+	char g{};
+	for (int i = 0; i < count; i++) {
+		fl.seekg(i * 268, ios::beg);
+		for (int y = 0; y < 64; y++) {
+			g = fl.get();
+			name_t[y] = g;
+			if (g == '\0')
+				break;
+		}
+		if (strcmp(name_t,name)==0) {
+			fl.seekg(i * 268, ios::beg);
+			fl.read(reinterpret_cast<char*>(&temp), sizeof(Directory));
+			temp.show();
+		}
+		
 	}
 	fl.close();
 }
